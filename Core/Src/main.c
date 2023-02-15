ae5af9d07290d4eafe3ce5101e8dd9769244e31c
BPPandi <115776528+BPPandi@users.noreply.github.com>
2023-02-15 17:44:15 +0530

    hi bldc

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,4 @@
-/* USER CODE BEGIN Header */
+/* USER CODE BEGIN Header */ //hi bldc
 /**
   ******************************************************************************
   * @file           : main.c
